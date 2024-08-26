47175e649356a873bd082335f74c573c5a34bb44
OnShubham <shubhamkharche3005@gmail.com>
2024-08-26 17:16:17 +0530

    addddd

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,14 @@ import path from "path"
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  build: {
+    sourcemap: mode !== 'production', // Disable source maps in production
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+}))
