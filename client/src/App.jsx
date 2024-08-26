bb1c1f9eafe8b1708512381ad806a769e5b0a8c5
OnShubham <shubhamkharche3005@gmail.com>
2024-08-26 16:25:09 +0530

    update the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,25 @@
-import { RouterProvider } from "react-router-dom";
-import router from "./routes/routes";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navigation from "./components/page/navigation";
 import Footer from "./components/navbar/footer";
+import Home from "./pages/home";
+import Pomodoro from "./components/page/pomodoro";
+import Error from "./components/page/error";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+
 function App() {
   return (
     <>
-      <Navigation />
-      <RouterProvider router={router} />
-      <hr style={{ borderTop: "2px solid #f97316" }} />
-      <Footer />
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />}  />
+          <Route path="/pomodoro" element={<Pomodoro />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
   );
 }
