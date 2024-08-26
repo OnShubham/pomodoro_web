ed91cceb9f74e321a44d98547d1cd79794c87ae9
OnShubham <shubhamkharche3005@gmail.com>
2024-08-26 17:53:50 +0530

    ad

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,9 +5,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: true,
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
