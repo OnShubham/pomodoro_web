90bf1063c970749caf5b2b3e8dbded3d37dfbcb6
OnShubham <shubhamkharche3005@gmail.com>
2024-08-26 16:16:26 +0530

    add

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "@/pages/home";
-import Login from "@/pages/login";
-import Signup from "@/pages/signup";
-import Error from "@/components/page/error";
-import Pomodoro from "@/components/page/pomodoro";
+import Home from "/pages/home";
+import Login from "/pages/login";
+import Signup from "/pages/signup";
+import Error from "/components/page/error";
+import Pomodoro from "/components/page/pomodoro";
 
 const router = createBrowserRouter([
   {
